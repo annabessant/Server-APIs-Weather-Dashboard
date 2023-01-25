e1c0d68c1c261e928e366066fff65f14d2ba1a78
annabessant <anna@thefutureofinnovation.org>
2023-01-25 20:05:05 +0000

    add href links

---
+++ script.js
@@ -0,0 +1,3 @@
+// Add your own API key & URL to api.openweather.map
+var APIKey = "k9kwhfWu.YHFZOslEQXaaUnDw0U3WLRkHs8RLPbWr"
+var queryURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey;
